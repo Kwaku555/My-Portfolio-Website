fbb39aba95f29e0332429fdf909ddd8a59275ffd
Kwaku555 <emmanuel7185@gmail.com>
2024-11-07 20:13:31 +0400

    Enhance mobile navigation styling and updated body content.
    1. Improved the navigation layout for a better mobile experience.
    
    2. Updated the content in the body section for clarity and relevance.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const hamburgers = document.querySelector('.hamburger');
 const menulinks = document.querySelector('.menu1');
 hamburgers.addEventListener('click',  () =>  { 
+    hamburgers.classList.toggle('active');
     menulinks.classList.toggle('active');
+
+    if (hamburger.classList.contains('active')) {
+        hamburger.innerHTML = '&#10005;'; // Change to "X"
+    } else {
+        hamburger.innerHTML = '&#9776;'; // Change back to hamburger
+    }
 });
